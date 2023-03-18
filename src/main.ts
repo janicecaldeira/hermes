291787649b7f25549daff7dc2e8b3547b4ca513c
Janice Caldeira <janicecaldeira@gmail.com>
2023-03-18 15:25:33 -0300

    feat: configure app to use global pipes and logger in all modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.useLogger(app.get(Logger));
+  
   await app.listen(3000);
+  console.log(`http://localhost:3000/`);
 }
 bootstrap();
